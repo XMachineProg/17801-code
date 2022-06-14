cb3ec69f928b71f9c5b4a23151dc07e11545f313
Antonio Souza <62897985+Ant0nioSouza@users.noreply.github.com>
2022-06-14 11:36:01 -0300

    feat: abstract StateMachine class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/model/StateMachine.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.model;
+
+import java.util.ArrayList;
+
+abstract class StateMachine {
+    public static State currentState;
+    private ArrayList<State> states = new ArrayList<State>();
+    
+}
