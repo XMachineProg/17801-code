da37bb30da5a20eee9791c23b48369e72da8fad5
Antonio Souza <62897985+Ant0nioSouza@users.noreply.github.com>
2022-06-14 15:44:55 -0300

    feat: state machine class to vertical claw motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateMachines/VCMachine.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.stateMachines;
+
+import org.firstinspires.ftc.teamcode.model.State;
+import org.firstinspires.ftc.teamcode.model.StateMachine;
+
+public class VCMachine extends StateMachine {
+    public static State currentState;
+    
+    class raisedClaw extends State {
+
+        @Override
+        protected void init() {
+
+        }
+
+        @Override
+        protected void loop() {
+
+        }
+    }
+    class loweredClaw extends State {
+
+        @Override
+        protected void init() {
+
+        }
+
+        @Override
+        protected void loop() {
+
+        }
+    }
+
+}
