1c770e83e9532531bfe631b0a503eb37714c2c15
Antonio Souza <62897985+Ant0nioSouza@users.noreply.github.com>
2022-06-14 11:48:08 -0300

    refactor: remove useless variable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/model/StateMachine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/model/StateMachine.java
@@ -3,7 +3,5 @@ package org.firstinspires.ftc.teamcode.model;
 import java.util.ArrayList;
 
 public abstract class StateMachine {
-    public static State currentState;
     private ArrayList<State> states = new ArrayList<State>();
-
 }
